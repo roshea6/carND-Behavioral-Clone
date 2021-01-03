bbbfbabae28acd8a14905de49f6cd4ad72d6a1cb
roshea6 <rpo1202@gmail.com>
2021-01-03 03:31:38 +0000

    Added model file base on an NVIDIA driverless car neural net. Almost makes it around the track

---
+++ model.py
@@ -0,0 +1,106 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D, Dense, Flatten, Lambda
+from keras.layers import MaxPooling2D, Cropping2D
+import numpy as np
+import cv2
+import csv
+
+# Lines from the csv file
+lines = []
+
+with open('./my_data/driving_log.csv') as csv_file:
+    # CSV file reader object
+    reader = csv.reader(csv_file)
+    for line in reader:
+        lines.append(line)
+        
+# Empty lists for our training images and steering angles
+images = []
+angles = []
+
+# Loop through all of the lines from the csv file
+for i, line in enumerate(lines):
+    # Skip the first iteration 
+    if i == 0:
+        continue
+        
+    # Get the center camera image
+    source_path = line[0]
+    # Split the path into a list of individual directories and the file name at the very end
+    filename = source_path.split('/')[-1]
+    img_path = './my_data/IMG/' + filename
+    img = cv2.imread(img_path)
+    images.append(img)
+    
+    # Get the stearing angle from the line and store it as a float
+    angles.append(float(line[3]))
+    
+    
+# Flip all the images and labels in the training set so the dataset has equal left and right turning examples
+aug_imgs = []
+aug_angles = []
+for img, angle in zip(images, angles):
+    # Append the normal image and angle
+    aug_imgs.append(img)
+    aug_angles.append(angle)
+    
+    # Append the flipped images and angles
+    aug_imgs.append(cv2.flip(img, 1))
+    aug_angles.append(angle * -1.0)
+    
+# Convert the images and angles to numpy arrays so they can be used to train the network
+x_train = np.array(images)
+y_train = np.array(angles)
+
+# Create the Nvidia driverless car model
+model = Sequential()
+
+# Input lambda layer to normalize and mean center the images
+model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160,320,3)))
+
+# Cropping layer to take only the ROI (Removes top 70 and bottom 25 pixels
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+# Convolution layer 1 with 24 5x5 filters
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+
+# Convolution layer 2 with 36 5x5 filters
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+
+# Convolution layer 3 with 48 5x5 filters
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+
+# Convolution layer 4 with 64 3x3 filters
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+
+# Convolution layer 5 with 64 3x3 filters
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+
+# Flatten the output of the last convolution layer so it can be connected with a fully connected layer
+model.add(Flatten())
+
+# Fully Connected layer 1
+model.add(Dense(100))
+
+# Fully Connected layer 2
+model.add(Dense(50))
+
+# Fully Connected layer 2
+model.add(Dense(10))
+
+# Output single node (The steering angle)
+model.add(Dense(1))
+
+# Compile the model with the proper loss function and optimizer
+model.compile(loss='mse', optimizer='adam')
+
+# Train the network
+model.fit(x_train, y_train, validation_split=.2, shuffle=True, epochs=5)
+
+model.save('model.h5')
+          
+      
+
+
+
+
